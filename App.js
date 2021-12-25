eb41eb941f107af99cfd661faf9a192704427ad0
ashir hussain <hussainashir87@gmail.com>
2021-12-26 01:33:05 +0500

    feat: add tabs and screens

--- App.js
+++ App.js
@@ -1,27 +1,75 @@
-import { Button, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react'
+import { 
+  StyleSheet,
+   Pressable,
+    Animated,
+     Dimensions,       StatusBar
+     } from 'react-native';
+import { SceneMap, TabView } from 'react-native-tab-view';
 import Chats from './Chats';
 import Status from './Status';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import Calls from './Calls';
+import { NativeBaseProvider, Box, Text, Center } from 'native-base';
+const renderScene = SceneMap({
+  first: Chats,
+  second: Status,
+  third: Calls
+
+});
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-  return (<>
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'Chats' },
+    { key: 'second', title: 'Status' },
+    { key: 'third', title: 'Calls' },
+  ]);
+  const initialLayout = { width: Dimensions.get('window').width };
+  const renderTabBar = (props) => {
+    const inputRange = props.navigationState.routes.map((x, i) => i);
+    return (
+      <Box flexDirection="row">
+        {props.navigationState.routes.map((route, i) => {
+          const opacity = props.position.interpolate({
+            inputRange,
+            outputRange: inputRange.map((inputIndex) =>
+              inputIndex === i ? 1 : 0.5
+            ),
+          });
+          const color = index === i ? '#1f2937' : '#a1a1aa';
+          const borderColor = index === i ? 'cyan.500' : 'coolGray.200';
 
-    <NavigationContainer>
-    <><Button title="Chats" /><Button title="Status"/>
-    <Button title="Calls" /></>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Chats"
-        component={Chats}
-        // options={{ title: 'Chats' }}
+          return (
+            <Box
+              borderBottomWidth="3"
+              borderColor={borderColor}
+              flex={1}
+              alignItems="center"
+              p="3">
+              <Pressable
+                onPress={() => {
+                  console.log(i);
+                  setIndex(i);
+                }}>
+                <Animated.Text style={{ color }}>{route.title}</Animated.Text>
+              </Pressable>
+            </Box>
+          );
+        })}
+      </Box>
+    );
+  };
+  return (
+    <NativeBaseProvider>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+        style={{ marginTop: StatusBar.currentHeight }}
       />
-      <Stack.Screen name="Status" component={Status} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  </>
+    </NativeBaseProvider>
   );
 }
 
